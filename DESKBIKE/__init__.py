476a09bd2a4c1e47e0f515e40dc5e588c933e831
Josua Burkard <josua.burkard@sdworx.com>
2024-06-18 11:45:32 +0200

    0.0.7

--- DESKBIKE/__init__.py
+++ DESKBIKE/__init__.py
@@ -4,7 +4,10 @@ from homeassistant.core import HomeAssistant
 
 from .parser import DeskBikeBluetoothDeviceData, DeskBikeDevice
 
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 
 __all__ = ["DeskBikeBluetoothDeviceData", "DeskBikeDevice"]
 
+_lastcsc01 = 0
+_lastcsc02 = 0
+# lastcsctime = datetime.now()
